9395f794bcd0a47a185a286643024976d8dfeeb6
Granberg Börje <borje.granberg@gmail.com>
2016-12-08 16:46:59 +0100

    Save a plot to file

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"time"
 
 	"github.com/felixge/pidctrl"
+	chart "github.com/wcharczuk/go-chart"
 )
 
 type system struct {
@@ -21,17 +24,68 @@ func (s *system) GetTemp() float64 {
 }
 
 func main() {
+	//
+	file, err := os.Create("output.png")
+	if err != nil {
+		log.Fatal("Cant create file")
+	}
+	const size = 12 * 4
+	//xvalues := make([]float64, size, size)
+	xvalues := make([]time.Time, size, size)
+	yvalues := make([]float64, size, size)
+	/*
+		graph := chart.Chart{
+			Series: []chart.Series{
+				chart.ContinuousSeries{
+					XValues: []float64{1.0, 2.0, 3.0, 4.0, 5.0},
+					YValues: []float64{1.0, 2.0, 3.0, 4.0, 5.0},
+				},
+			},
+		}
+	*/
+
 	garage := system{.025, 19}
 	//	c := pidctrl.NewPIDController(20, .1, 0)
-	c := pidctrl.NewPIDController(20, .1, 0)
+	c := pidctrl.NewPIDController(20, .05, 0)
 	c.Set(20)
 	c.SetOutputLimits(0, 5)
 	duration := time.Minute * 5
-	for {
+	theTime := time.Now()
+	for i := 0; i < size; i++ {
+		//xvalues[i] = float64(i)
+		xvalues[i] = theTime
+		yvalues[i] = garage.GetTemp()
+
 		output := c.UpdateDuration(garage.GetTemp(), duration)
-		garage.Update(output * .1 / 5 * 2000 / 750)
+		garage.Update(output * .1 / 5)
 		fmt.Printf("Garage: %.2f\tWarmup: %.1f\n", garage.GetTemp(), output)
-		time.Sleep(time.Second)
+		//time.Sleep(time.Second)
+		theTime = theTime.Add(duration)
+	}
+	graph := chart.Chart{
+		XAxis: chart.XAxis{
+			Style: chart.Style{
+				Show: true,
+			},
+			TickPosition:   chart.TickPositionBetweenTicks,
+			ValueFormatter: chart.TimeMinuteValueFormatter,
+		},
+		YAxis: chart.YAxis{
+			Style: chart.Style{
+				Show: true,
+			},
+		},
+		Series: []chart.Series{
+			chart.TimeSeries{
+				XValues: xvalues,
+				YValues: yvalues,
+			},
+		},
 	}
 
+	err = graph.Render(chart.PNG, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Close()
 }
